8067eff8626739761f2403833d9afcc58f285d88
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-06-03 15:16:39 +0530

    refactor: improve code formatting and structure in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,36 @@
 import express from "express";
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import { createServer } from "http";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server,{
-    cors:{
-        origin:"http://localhost:5173",
-        credentials:true
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
 
-io.on("connection",(socket)=>{
-    console.log("User Connected:",socket.id);
-    socket.on("join-room",(roomId)=>{
-        socket.join(roomId);
-        console.log(`${socket.id} joined room ${roomId}`)
-    })
+io.on("connection", (socket) => {
+  console.log("User Connected:", socket.id);
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    console.log(`${socket.id} joined room ${roomId}`);
+  });
+
+  socket.on("send-message", ({ roomId, message, user }) => {
+    io.to(roomId).emit("recevie-message", {
+      message,
+      user,
+      socketId: socket.id,
+    });
+  });
+
+socket.on("disconnect",() => {
+     console.log("User disconnected:", socket.id);
+})
+});
+
+server.listen(3000,()=>{
+    console.log("Server is running on port 3000");
 })
